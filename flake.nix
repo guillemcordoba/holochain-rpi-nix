c203bf3e539058854d15a33bffa761b6409c5df9
guillem.cordoba <guillem.cordoba@gmail.com>
2024-11-29 15:26:44 +0100

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Template for Holochain app development";
+
+  inputs = { holonix.url = "github:holochain/holonix/main-0.3"; };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://holochain-ci.cachix.org"
+      "https://darksoil-studio.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8="
+      "darksoil-studio.cachix.org-1:UEi+aujy44s41XL/pscLw37KEVpTEIn8N/kn7jO8rkc="
+    ];
+  };
+
+  outputs = inputs@{ ... }:
+    inputs.holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = builtins.attrNames inputs.holonix.devShells;
+      perSystem = { inputs', config, pkgs, system, lib, ... }: {
+        packages.holochain = inputs'.holonix.packages.holochain.override {
+          cargoExtraArgs =
+            " --no-default-features --features sqlite-encrypted,tx5";
+        };
+      };
+    };
+}
